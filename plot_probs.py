42289d44d9de8e03da6bac12df21482ec0258a7c
evitali <evitali@ghgsat.com>
2019-10-02 11:39:37 -0400

    changed plotting module to plot each individually and updated probabilities algorithm

--- plot_probs.py
+++ plot_probs.py
@@ -1,48 +1,117 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.special import comb
 
-#fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10) = plt.subplots(nrows = 5, ncols = 2, sharex = True)
+def ncr(n, k):
+	return comb(n, k, exact = False)
 
 iters, nones, pairs, twoPairs, threeKinds, houses, fourKinds, straights, flushes, sFlushes, rFlushes = np.loadtxt('probabilites.txt', delimiter = ',', unpack = True, skiprows = 2)
+iters1, nones1, pairs1, twoPairs1, threeKinds1, houses1, fourKinds1, straights1, flushes1, sFlushes1, rFlushes1 = np.loadtxt('actuals.txt', delimiter = ',', unpack = True, skiprows = 2)
 
 probs = [nones, pairs, twoPairs, threeKinds, houses, fourKinds, straights, flushes, sFlushes, rFlushes]
+probs1 = [nones1, pairs1, twoPairs1, threeKinds1, houses1, fourKinds1, straights1, flushes1, sFlushes1, rFlushes1]
 
-fig, ax = plt.subplots(nrows = 5, ncols = 2, sharex = True)
 
-ax[0][0].plot(iters, probs[0], 'r')
-ax[0][1].plot(iters, probs[1], 'r')
-ax[1][0].plot(iters, probs[2], 'r')
-ax[1][1].plot(iters, probs[3], 'r')
-ax[2][0].plot(iters, probs[4], 'r')
-ax[2][1].plot(iters, probs[5], 'r')
-ax[3][0].plot(iters, probs[6], 'r')
-ax[3][1].plot(iters, probs[7], 'r')
-ax[4][0].plot(iters, probs[8], 'r')
-ax[4][1].plot(iters, probs[9], 'r')
+freq_tot = ncr(52,5)
+rFlush_th = 100*ncr(4,1)/freq_tot
+sFlush_th = 100*(ncr(10,1)*ncr(4,1)-ncr(4,1))/freq_tot
+fourKind_th = 100*ncr(13,1)*ncr(4,1)*ncr(12,1)/freq_tot
+fullHouse_th = 100*ncr(13,1)*ncr(4,3)*ncr(12,1)*ncr(4,2)/freq_tot
+flush_th = 100*(ncr(13,5)*ncr(4,1) - ncr(10,1)*ncr(4,1))/freq_tot
+straight_th = 100*(ncr(10,1)*(ncr(4,1)**5) - ncr(10,1)*ncr(4,1))/freq_tot
+threeKind_th = 100*ncr(13,1)*ncr(4,3)*ncr(12,2)*(ncr(4,1)**2)/freq_tot
+twoPair_th = 100*ncr(13,2)*(ncr(4,2)**2)*ncr(11,1)*ncr(4,1)/freq_tot
+pair_th = 100*ncr(13,1)*ncr(4,2)*ncr(12,3)*(ncr(4,1)**3)/freq_tot
+none_th = 100*((ncr(13,5) - 10)*((ncr(4,1)**5) - 4))/freq_tot
 
-plt.tight_layout()
-fig.ylabel("Number of Hands")
-fig.xlabel("Hands dealt")
-'''
-ax1.plot(iters, nones, 'r')
-ax2.plot(iters, pairs, 'r')
-ax3.plot(iters, twoPairs, 'r')
-ax4.plot(iters, threeKinds, 'r')
-ax5.plot(iters, houses, 'r')
-ax6.plot(iters, fourKinds, 'r')
-ax7.plot(iters, straights, 'r')
-ax8.plot(iters, flushes, 'r')
-ax9.plot(iters, sFlushes, 'r')
-ax10.plot(iters, rFlushes, 'r')
-'''
-plt.savefig('cards.png', dpi = 250)
-'''
-#plt.plot(iters, straights, 'r', label = 'Actual')
-#plt.plot(iters, straights_th, 'b', label = 'Theoretical')
-plt.legend(loc = 'upper right')
-plt.xlabel('Iterations')
-plt.ylabel('Number of straights')
-plt.title("Straights")
-plt.savefig('straights.png', dpi = 300)
-plt.show()
-'''
+n = float(len(iters1))/100
+
+plt.figure()
+plt.plot(iters, probs[0], 'r', label = 'Theoretical: %.6f %%'%(none_th))
+plt.plot(iters, probs1[0], 'b', label = 'Actual: %.6f %%'%(probs1[0][-1]/n))
+plt.xlabel('# of Hands dealt')
+plt.ylabel('# of no-pairs')
+plt.title('No Pair')
+plt.legend(loc = 'upper left')
+plt.savefig('plots/nones.png', dpi = 250)
+
+plt.figure()
+plt.plot(iters, probs[1], 'r', label = 'Theoretical: %.6f %%'%(pair_th))
+plt.plot(iters, probs1[1], 'b', label = 'Actual: %.6f %%'%(probs1[1][-1]/n))
+plt.title('Pair')
+plt.xlabel('# of Hands dealt')
+plt.ylabel('# of Pairs')
+plt.legend(loc = 'upper left')
+plt.savefig('plots/pairs.png', dpi = 250)
+
+plt.figure()
+plt.plot(iters, probs[2], 'r', label = 'Theoretical: %.6f %%'%(twoPair_th))
+plt.plot(iters, probs1[2], 'b', label = 'Actual: %.6f %%'%(probs1[2][-1]/n))
+plt.title('Two-Pair')
+plt.xlabel('# of Hands dealt')
+plt.ylabel('# of Two-Pairs')
+plt.legend(loc = 'upper left')
+plt.savefig('plots/twoPairs.png', dpi = 250)
+
+plt.figure()
+plt.plot(iters, probs[3], 'r', label = 'Theoretical: %.6f %%'%(threeKind_th))
+plt.plot(iters, probs1[3], 'b', label = 'Actual: %.6f %%'%(probs1[3][-1]/n))
+plt.title('Three-of-a-Kind')
+plt.xlabel('# of Hands dealt')
+plt.ylabel('# of Three-of-a-Kinds')
+plt.legend(loc = 'upper left')
+plt.savefig('plots/threeKinds.png', dpi = 250)
+
+plt.figure()
+plt.plot(iters, probs[4], 'r', label = 'Theoretical: %.6f %%' %(fullHouse_th))
+plt.plot(iters, probs1[4], 'b', label = 'Actual: %.6f %%'%(probs1[4][-1]/n))
+plt.title('Full House')
+plt.xlabel('# of Hands dealt')
+plt.ylabel('# of Full-Houses')
+plt.legend(loc = 'upper left')
+plt.savefig('plots/houses.png', dpi = 250)
+
+plt.figure()
+plt.plot(iters, probs[5], 'r', label = 'Theoretical: %.6f %%' %(fourKind_th))
+plt.plot(iters, probs1[5], 'b', label = 'Actual: %.6f %%'%(probs1[5][-1]/n))
+plt.title('Four-of-a-Kind')
+plt.xlabel('# of Hands dealt')
+plt.ylabel('# of Four-of-a-Kinds')
+plt.legend(loc = 'upper left')
+plt.savefig('plots/fourKinds.png', dpi = 250)
+
+plt.figure()
+plt.plot(iters, probs[6], 'r', label = 'Theoretical: %.6f %%'%(straight_th))
+plt.plot(iters, probs1[6], 'b', label = 'Actual: %.6f %%'%(probs1[6][-1]/n))
+plt.title('Straight')
+plt.xlabel('# of Hands dealt')
+plt.ylabel('# of Straights')
+plt.legend(loc = 'upper left')
+plt.savefig('plots/straights.png', dpi = 250)
+
+plt.figure()
+plt.plot(iters, probs[7], 'r', label = 'Theoretical: %.8f %%'%(flush_th))
+plt.plot(iters, probs1[7], 'b', label = 'Actual: %.8f %%'%(probs1[7][-1]/n))
+plt.title('Flush')
+plt.xlabel('# of Hands dealt')
+plt.ylabel('# of Flushes')
+plt.legend(loc = 'upper left')
+plt.savefig('plots/flushes.png', dpi = 250)
+
+plt.figure()
+plt.plot(iters, probs[8], 'r', label = 'Theoretical: %.9f %%'%(sFlush_th))
+plt.plot(iters, probs1[8], 'b', label = 'Actual: %.9f %%'%(probs1[8][-1]/n))
+plt.title('Straight Flush')
+plt.xlabel('# of Hands dealt')
+plt.ylabel('# of Straight Flushes')
+plt.legend(loc = 'upper left')
+plt.savefig('plots/sFlushes.png', dpi = 250)
+
+plt.figure()
+plt.plot(iters, probs[9], 'r', label = 'Theoretical: %.9f %%'%(rFlush_th))
+plt.plot(iters, probs1[9], 'b', label = 'Actual: %.9f %%'%(probs1[9][-1]/n))
+plt.title('Royal Flush')
+plt.xlabel('# of Hands dealt')
+plt.ylabel('# of Royal Flushes')
+plt.legend(loc = 'upper left')
+plt.savefig('plots/rFlushes.png', dpi = 250)
--- poker.py
+++ poker.py
@@ -5,100 +5,100 @@ import matplotlib.pylab as plt
 
 rank_dict = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
 def init_deck():
-	ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
-	suits = ['S', 'D', 'H', 'C']
-	deck = []
-	for i in range(len(ranks)):
-		for j in range(len(suits)):
-			deck.append(suits[j]+ranks[i])
-	return deck
+    ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K']
+    suits = ['S', 'D', 'H', 'C']
+    deck = []
+    for i in range(len(ranks)):
+        for j in range(len(suits)):
+            deck.append(suits[j]+ranks[i])
+    return deck
 
 def is_ordered(ranks):
-	return sorted(ranks) == list(range(min(ranks), max(ranks) + 1))
+    return sorted(ranks) == list(range(min(ranks), max(ranks) + 1))
 
 def ncr(n, k):
-	return comb(n, k, exact = False)
+    return comb(n, k, exact = False)
 
 
 def is_royalFlush(hand):
-	hand_ranks = [i[1] for i in hand]
-	ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
-	hand_suits = [i[0] for i in hand]
-	if(len(list(dict.fromkeys(hand_suits))) == 1 and 'A' in hand_ranks and 'T' in hand_ranks and 'J' in hand_ranks and 'Q' in hand_ranks and 'K' in hand_ranks):
-		return True
-	else:
-		return False
+    hand_ranks = [i[1] for i in hand]
+    ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
+    hand_suits = [i[0] for i in hand]
+    if(len(list(dict.fromkeys(hand_suits))) == 1 and 'A' in hand_ranks and 'T' in hand_ranks and 'J' in hand_ranks and 'Q' in hand_ranks and 'K' in hand_ranks):
+        return True
+    else:
+        return False
 
 def is_straightFlush(hand):
-	hand_ranks = [i[1] for i in hand]
-	ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
-	hand_suits = [i[0] for i in hand]
-	if(len(list(dict.fromkeys(hand_suits))) == 1 and is_ordered(ranks_num)):
-		return True
-	else:
-		return False
+    hand_ranks = [i[1] for i in hand]
+    ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
+    hand_suits = [i[0] for i in hand]
+    if(len(list(dict.fromkeys(hand_suits))) == 1 and is_ordered(ranks_num)):
+        return True
+    else:
+        return False
 
 def is_fourKind(hand):
-	hand_ranks = [i[1] for i in hand]
-	ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
-	ranks_num = sorted(ranks_num)
-	#has to either be 4 cards of same rank + higher unmatched card or 4 cards of same rank + lower unmatched card
-	result1 = ranks_num[0] == ranks_num[1] and ranks_num[1] == ranks_num[2] and ranks_num[2] == ranks_num[3]
-	result2 = ranks_num[1] == ranks_num[2] and ranks_num[2] == ranks_num[3] and ranks_num[3] == ranks_num[4]
-	return (result1 or result2)
+    hand_ranks = [i[1] for i in hand]
+    ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
+    ranks_num = sorted(ranks_num)
+    #has to either be 4 cards of same rank + higher unmatched card or 4 cards of same rank + lower unmatched card
+    result1 = ranks_num[0] == ranks_num[1] and ranks_num[1] == ranks_num[2] and ranks_num[2] == ranks_num[3]
+    result2 = ranks_num[1] == ranks_num[2] and ranks_num[2] == ranks_num[3] and ranks_num[3] == ranks_num[4]
+    return (result1 or result2)
 
 def is_fullHouse(hand):
-	hand_ranks = [i[1] for i in hand]
-	ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
-	ranks_num = sorted(ranks_num)
-	result1 = ranks_num[0] == ranks_num[1] and ranks_num[1] == ranks_num[2] and ranks_num[3] == ranks_num[4]
-	result2 = ranks_num[0] == ranks_num[1] and ranks_num[2] == ranks_num[3] and ranks_num[3] == ranks_num[4]
-	return (result1 or result2)
+    hand_ranks = [i[1] for i in hand]
+    ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
+    ranks_num = sorted(ranks_num)
+    result1 = ranks_num[0] == ranks_num[1] and ranks_num[1] == ranks_num[2] and ranks_num[3] == ranks_num[4]
+    result2 = ranks_num[0] == ranks_num[1] and ranks_num[2] == ranks_num[3] and ranks_num[3] == ranks_num[4]
+    return (result1 or result2)
 
 
 def is_flush(hand):
-	hand_suits = [i[0] for i in hand]
-	if(len(list(dict.fromkeys(hand_suits))) == 1):
-		return True
-	else:
-		return False
+    hand_suits = [i[0] for i in hand]
+    if(len(list(dict.fromkeys(hand_suits))) == 1):
+        return True
+    else:
+        return False
 
 def is_straight(hand):
-	hand_ranks = [i[1] for i in hand]
-	ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
-	if is_ordered(ranks_num):
-		return True
-	else:
-		return False
+    hand_ranks = [i[1] for i in hand]
+    ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
+    if is_ordered(ranks_num):
+        return True
+    else:
+        return False
 
 def is_threeKind(hand):
-	hand_ranks = [i[1] for i in hand]
-	ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
-	ranks_num = sorted(ranks_num)
-	result1 = ranks_num[0] == ranks_num[1] and ranks_num[1] == ranks_num[2] and ranks_num[3] != ranks_num[0] and ranks_num[4] != ranks_num[0] and ranks_num[3] != ranks_num[4]
-	result2 = ranks_num[1] == ranks_num[2] and ranks_num[2] == ranks_num[3] and ranks_num[0] != ranks_num[1] and ranks_num[0] != ranks_num[4] and ranks_num[4] != ranks_num[1]
-	result3 = ranks_num[2] == ranks_num[3] and ranks_num[3] == ranks_num[4] and ranks_num[0] != ranks_num[2] and ranks_num[0] != ranks_num[1] and ranks_num[1] != ranks_num[2]
-	return (result1 or result2 or result3)
+    hand_ranks = [i[1] for i in hand]
+    ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
+    ranks_num = sorted(ranks_num)
+    result1 = ranks_num[0] == ranks_num[1] and ranks_num[1] == ranks_num[2] and ranks_num[3] != ranks_num[0] and ranks_num[4] != ranks_num[0] and ranks_num[3] != ranks_num[4]
+    result2 = ranks_num[1] == ranks_num[2] and ranks_num[2] == ranks_num[3] and ranks_num[0] != ranks_num[1] and ranks_num[0] != ranks_num[4] and ranks_num[4] != ranks_num[1]
+    result3 = ranks_num[2] == ranks_num[3] and ranks_num[3] == ranks_num[4] and ranks_num[0] != ranks_num[2] and ranks_num[0] != ranks_num[1] and ranks_num[1] != ranks_num[2]
+    return (result1 or result2 or result3)
 
 def is_twoPair(hand):
-	hand_ranks = [i[1] for i in hand]
-	ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
-	ranks_num = sorted(ranks_num)
-	result1 = ranks_num[1] == ranks_num[2] and ranks_num[3] == ranks_num[4] and ranks_num[0] != ranks_num[1] and ranks_num[0] != ranks_num[3]
-	result2 = ranks_num[0] == ranks_num[1] and ranks_num[3] == ranks_num[4] and ranks_num[2] != ranks_num[0] and ranks_num[2] != ranks_num[3]
-	result3 = ranks_num[0] == ranks_num[1] and ranks_num[2] == ranks_num[3] and ranks_num[4] != ranks_num[0] and ranks_num[4] != ranks_num[2]
-	return (result1 or result2 or result3)
+    hand_ranks = [i[1] for i in hand]
+    ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
+    ranks_num = sorted(ranks_num)
+    result1 = ranks_num[1] == ranks_num[2] and ranks_num[3] == ranks_num[4] and ranks_num[0] != ranks_num[1] and ranks_num[0] != ranks_num[3]
+    result2 = ranks_num[0] == ranks_num[1] and ranks_num[3] == ranks_num[4] and ranks_num[2] != ranks_num[0] and ranks_num[2] != ranks_num[3]
+    result3 = ranks_num[0] == ranks_num[1] and ranks_num[2] == ranks_num[3] and ranks_num[4] != ranks_num[0] and ranks_num[4] != ranks_num[2]
+    return (result1 or result2 or result3)
 
 def is_pair(hand):
-	hand_ranks = [i[1] for i in hand]
-	ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
-	ranks_num = sorted(ranks_num)
-	if(is_fourKind(hand) or is_fullHouse(hand) or is_threeKind(hand) or is_twoPair(hand)): return False
-	result1 = ranks_num[0] == ranks_num[1]
-	result2 = ranks_num[1] == ranks_num[2]
-	result3 = ranks_num[2] == ranks_num[3]
-	result4 = ranks_num[3] == ranks_num[4]
-	return (result1 or result2 or result3 or result4)
+    hand_ranks = [i[1] for i in hand]
+    ranks_num = [rank_dict[hand_ranks[i]] for i in range(len(hand_ranks))]
+    ranks_num = sorted(ranks_num)
+    if(is_fourKind(hand) or is_fullHouse(hand) or is_threeKind(hand) or is_twoPair(hand)): return False
+    result1 = ranks_num[0] == ranks_num[1]
+    result2 = ranks_num[1] == ranks_num[2]
+    result3 = ranks_num[2] == ranks_num[3]
+    result4 = ranks_num[3] == ranks_num[4]
+    return (result1 or result2 or result3 or result4)
 
 num_rFlush = 0
 num_sFlush = 0
@@ -120,11 +120,10 @@ flush_th = (ncr(13,5)*ncr(4,1) - ncr(10,1)*ncr(4,1))/freq_tot
 straight_th = (ncr(10,1)*(ncr(4,1)**5) - ncr(10,1)*ncr(4,1))/freq_tot
 threeKind_th = ncr(13,1)*ncr(4,3)*ncr(12,2)*(ncr(4,1)**2)/freq_tot
 twoPair_th = ncr(13,2)*(ncr(4,2)**2)*ncr(11,1)*ncr(4,1)/freq_tot
-pair_th = ncr(13,1)*ncr(4,2)*ncr(12,3)*(ncr(4,2)**3)/freq_tot
+pair_th = ncr(13,1)*ncr(4,2)*ncr(12,3)*(ncr(4,1)**3)/freq_tot
 none_th = ((ncr(13,5) - 10)*((ncr(4,1)**5) - 4))/freq_tot
 
-n = 1e5
-
+n = 1e6
 iters = []
 nones = []
 nones_th = []
@@ -148,64 +147,65 @@ rFlushes = []
 rFlushes_th = []
 
 start = time.clock()
+
 for i in range(int(n)):
-	hand = ran.sample(init_deck(), 5)
-	if is_royalFlush(hand):
-		num_rFlush += 1
-	elif is_straightFlush(hand):
-		num_sFlush += 1
-	elif is_fourKind(hand):
-		num_fourKind += 1
-	elif is_fullHouse(hand):
-		num_fullHouse += 1
-	elif is_flush(hand):
-		num_flush += 1
-	elif is_straight(hand):
-		num_straight += 1
-	elif is_threeKind(hand):
-		num_threeKind += 1
-	elif is_twoPair(hand):
-		num_twoPair += 1
-	elif is_pair(hand):
-		num_pair += 1
-	else:
-		num_none += 1
-	
-	iters.append(float(i))
-	nones.append(num_none)
-	nones_th.append(float(i)*none_th)
-	pairs.append(num_pair)
-	pairs_th.append(float(i)*pair_th)
-	twoPairs.append(num_twoPair)
-	twoPairs_th.append(float(i)*twoPair_th)
-	threeKinds.append(num_threeKind)
-	threeKinds_th.append(float(i)*threeKind_th)
-	houses.append(num_fullHouse)
-	houses_th.append(float(i)*fullHouse_th)
-	fourKinds.append(num_fourKind)
-	fourKinds_th.append(float(i)*fourKind_th)
-	flushes.append(num_flush)
-	flushes_th.append(float(i)*flush_th)
-	straights.append(num_straight)
-	straights_th.append(float(i)*straight_th)
-	sFlushes.append(num_sFlush)
-	sFlushes_th.append(float(i)*sFlush_th)
-	rFlushes.append(num_rFlush)
-	rFlushes_th.append(float(i)*rFlush_th)
+    
+    hand = ran.sample(init_deck(), 5)
+
+    if is_royalFlush(hand):
+        num_rFlush += 1
+    
+    elif is_straightFlush(hand):
+        num_sFlush += 1
+    
+    elif is_fourKind(hand):
+        num_fourKind += 1
+    
+    elif is_fullHouse(hand):
+        num_fullHouse += 1
+    
+    elif is_flush(hand):
+        num_flush += 1
+    
+    elif is_straight(hand):
+        num_straight += 1
+    
+    elif is_threeKind(hand):
+        num_threeKind += 1
+    
+    elif is_twoPair(hand):
+        num_twoPair += 1
+    
+    elif is_pair(hand):
+        num_pair += 1
+    
+    else:
+        num_none += 1
+    
+    iters.append(float(i))
+    nones.append(num_none)
+    nones_th.append(float(i)*none_th)
+    pairs.append(num_pair)
+    pairs_th.append(float(i)*pair_th)
+    twoPairs.append(num_twoPair)
+    twoPairs_th.append(float(i)*twoPair_th)
+    threeKinds.append(num_threeKind)
+    threeKinds_th.append(float(i)*threeKind_th)
+    houses.append(num_fullHouse)
+    houses_th.append(float(i)*fullHouse_th)
+    fourKinds.append(num_fourKind)
+    fourKinds_th.append(float(i)*fourKind_th)
+    flushes.append(num_flush)
+    flushes_th.append(float(i)*flush_th)
+    straights.append(num_straight)
+    straights_th.append(float(i)*straight_th)
+    sFlushes.append(num_sFlush)
+    sFlushes_th.append(float(i)*sFlush_th)
+    rFlushes.append(num_rFlush)
+    rFlushes_th.append(float(i)*rFlush_th)
 
 end = time.clock()
 proc_t = end - start
-
-print(f"Dealing {n} hands took {proc_t} seconds!")
-
-print(f"len(iters) = {len(iters)}")
-fname = 'probabilites.txt'
-with open(fname, 'w') as out_file:
-	out_file.write("iters,none,pair,twoPair,threeKind,fullHouse,fourKind,straight,flush,sFlush,rFlush\n")
-	for i in range(len(iters)):
-		print(i)
-		out_file.write(str(iters[i]) + ',' + str(nones_th[i])+ ',' + str(pairs_th[i]) + ',' + str(twoPairs_th[i]) + ','+str(threeKinds_th[i]) + ',' + str(houses_th[i]) + ',' + str(fourKinds_th[i]) + ',' + str(straights_th[i]) + ',' + str(flushes_th[i]) + ',' + str(sFlushes_th[i]) + ',' + str(rFlushes_th[i]) + '\n')
-
 
 prob_rFlush = 100*num_rFlush/float(n)
 prob_sFlush = 100*num_sFlush/float(n)
@@ -217,5 +217,24 @@ prob_threeKind = 100*num_threeKind/float(n)
 prob_twoPair = 100*num_twoPair/float(n)
 prob_pair = 100*num_pair/float(n)
 prob_none = 100*num_none/float(n)
+
+print(f"Dealing {n} hands took {proc_t} seconds!")
+
+print(f"len(iters) = {len(iters)}")
+
+fname = 'probabilites.txt'
+with open(fname, 'w') as out_file:
+    out_file.write("iters,none,pair,twoPair,threeKind,fullHouse,fourKind,straight,flush,sFlush,rFlush\n")
+    for i in range(len(iters)):
+	    out_file.write(str(iters[i]) + ',' + str(nones_th[i])+ ',' + str(pairs_th[i]) + ',' + str(twoPairs_th[i]) + ','+str(threeKinds_th[i]) + ',' + str(houses_th[i]) + ',' + str(fourKinds_th[i]) + ',' + str(straights_th[i]) + ',' + str(flushes_th[i]) + ',' + str(sFlushes_th[i]) + ',' + str(rFlushes_th[i]) + '\n')
+
+
+fname2 = 'actuals.txt'
+with open(fname2, 'w') as out_file2:
+    out_file2.write("iters,none,pair,twoPair,threeKind,fullHouse,fourKind,straight,flush,sFlush,rFlush\n")
+    for i in range(len(iters)):
+	    out_file2.write(str(iters[i]) + ',' + str(nones[i])+ ',' + str(pairs[i]) + ',' + str(twoPairs[i]) + ','+str(threeKinds[i]) + ',' + str(houses[i]) + ',' + str(fourKinds[i]) + ',' + str(straights[i]) + ',' + str(flushes[i]) + ',' + str(sFlushes[i]) + ',' + str(rFlushes[i]) + '\n')
+
+
 
 print("Done!")
